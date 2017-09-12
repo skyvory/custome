43e3203c773c7c00d7b27b5f794034a0dd4eb6d2
Rika Voice <skyvory@hotmail.com>
2017-09-12 12:33:40 +0800

    Working hash and rename obfuscator

--- filename-obfuscaton/hash.bat
+++ filename-obfuscaton/hash.bat
@@ -1,7 +1,50 @@
 @echo off
-setlocal enableDelayedExpansion
+setlocal enableextensions enabledelayedexpansion
 
-fciv -add "a.jpg" -sha1 > temp.txt
+set directory=%~dp0
+
+:again
+if "%~1" == "" goto done
+
+echo "%~n1"
+echo "%~x1"
+
+REM echo "%~1"
+cd /d !directory!
+fciv -add "%~1" -sha1 > temp~~~.txt
+
+set /a iteration = 0
+for /F "tokens=*" %%A in (temp~~~.txt) do (
+  set /a iteration += 1
+  if !iteration! == 4 (
+    echo %%A
+    set str=%%A
+    set cut=!str:~0,40!
+    echo !cut!
+  )
+)
+
+echo !cut!
+
+REM rename files to hash value
+ren "%~1" "!cut!%~x1"
+echo "new name: %~dp1!cut!%~x1"
+
+REM rename txt to original file name
+ren "temp~~~.txt" "%~nx1.inf"
+move /Y "%~nx1.inf" "%~dp1"
+
+
+shift
+goto again
+:done
+pause
+exit
+
+REM cd /d %1
+echo Working on %~dp0
+
+%~dp0fciv -add "%1a.jpg" -sha1 > temp.txt
 
 set /a iteration = 0
 for /F "tokens=*" %%A in (temp.txt) do (
