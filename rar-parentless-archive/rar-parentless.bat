a710aae76e51133d9d5666120d4bcae65837a1a9
Rika Voice <skyvory@hotmail.com>
2021-10-17 23:25:54 +0800

    Fix folder creation issue when filename contains space

--- rar-parentless-archive/rar-parentless.bat
+++ rar-parentless-archive/rar-parentless.bat
@@ -1,7 +1,7 @@
-@echo on
+@echo off
 for /d %%G in (*) do (
   @REM cd %%G
-  rar a -r -ep1 -s -rr1p -t  -ts -x*Thumbs.db %%G.rar "%%G\*"
+  rar a -r -ep1 -s -rr1p -t  -ts -x*Thumbs.db "%%G.rar" "%%G\*"
   @REM cd ..
 )
-@REM pause
+pause
